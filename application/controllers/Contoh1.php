80d92eb863c26451e30f51e8a587cd8d146deeff
ademanis <ademanis758@gmail.com>
2023-09-26 20:47:25 +0700

    Membuat project CI Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+    echo "<h1> Perkenalan </hi>";
+    echo "Nama Saya Ade Kurnia
+    Saya Tinggal di Daerah Cengkareng
+    Olah Raga yang Saya sukai adalah Sepak Bola";
+}
+
+}
